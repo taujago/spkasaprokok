dfe96a7976c345c58fbea40877d42b3a6bffa77d
Aras Nediansyah <arazesyah@gmail.com>
2019-06-24 14:26:27 -0700

    Home New

--- application/modules/home/views/home_view.php
+++ application/modules/home/views/home_view.php
@@ -2,10 +2,9 @@
 	<div class="col-md-12">
 		<center>
 			<h3>
-				Selamat Datang Di Sistem Pakar Mendiagnosa Penyakit Yang Disebabkan Oleh Asap Rokok
+				Selamat Datang Di Sistem Pakar Mendiagnosa Penyakit Yang Disebabkan Oleh Asap Rokok Menggunakan Metode Case Based Reasoning (CBR)
 			</h3>
 			<p>
-	dibuat oleh aras 
 			</p>
 		</center>
 	</div>
